295a88cfcbe927c64d214b748ac1dab1ff8ed843
fanbeatsman <fan_ma8@hotmail.com>
2016-11-03 23:26:42 -0400

    setup react, babel and webpack, BONUS jade files for an idea i might or might not do

--- app.js
+++ app.js
@@ -2,18 +2,15 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-//var router = app.Router();
-
-//var db = mongoose.connection;
 var routes = require('./routes/myRoute');
-//mongoose.connect('mongodb://localhost/SwimComments');
-
 
 
 app.set('views', path.join(__dirname, 'public'));
 app.set('view engine', 'jade');
 app.use('/', routes);
-//app.use(express.static(path.join(__dirname, 'public/')));
+
+//Should narrow this path of static files to serve, or create a route or something else.
+app.use(express.static(path.join(__dirname, 'public/')));
 
 //app.use(express.static(__dirname + '/public'));
 app.listen(3000);
